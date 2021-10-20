9b0b12b9f68e268baa2386f5bff48fb9474ffdaf
Adam Doe <adamnickolasdoe@gmail.com>
2021-10-19 21:46:51 -0700

    Bar Chart > Add X & Y Scales

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -3,31 +3,36 @@ import * as d3 from 'd3';
 export default class D3Chart {
 	constructor(element) {
 
-		const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
-		const width = 800;
-		const height = 500;
+		const CHART_DATA = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
+		const CHART_WIDTH = 800;
+		const CHART_HEIGHT = 500;
 
 		const svg = d3.select(element)
-						.append('svg')
-						.attr('width', width)
-						.attr('height', height);
+			.append('svg')
+			.attr('width', CHART_WIDTH)
+			.attr('height', CHART_HEIGHT);
 		
-			d3.json(url).then(data => {
-				const rects = svg.selectAll('rect')
-								.data(data);
-					
-				rects.enter()
-					.append('rect')
-						.attr("x", (d, i) => i * 100)
-						.attr("y", 50)
-						.attr("width", 50)
-						.attr("height", d => d.height)
-						.attr("fill", d => {
-							if (d.height > 260) {
-								return "red"
-							}
-							return "green"
-						})
+		d3.json(CHART_DATA).then(data => {
+			
+			const y = d3.scaleLinear()
+				.domain([0, d3.max( data, d => d.height )])
+				.range([0, CHART_HEIGHT]);
+
+			const x = d3.scaleBand()
+				.domain(data.map( d => d.name))
+				.range([0, CHART_WIDTH])
+				.padding(0.2)
+
+			const rects = svg.selectAll('rect')
+				.data(data);
+				
+			rects.enter()
+				.append('rect')
+					.attr("x", (d, i) => x(d.name))
+					.attr("y", d => CHART_HEIGHT - y(d.height) )
+					.attr("width", x.bandwidth)
+					.attr("height", d => y(d.height))
+					.attr("fill", "gray")
 			})
 	}
 }
